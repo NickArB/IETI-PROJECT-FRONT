384bb1ad909cad32795a84eb88487cdc0a6a1c05
Reaper <nicolas.ariza241@gmail.com>
2024-04-11 11:18:36 -0500

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Bienvenido a AGRO-GNOME</h1>
+        <h3>Las mejores soluciones para su producción y rentabilidad!</h3>
+    </div>
+  );
+}
---
+++ src/components/menu.jsx
@@ -0,0 +1,8 @@
+export default function menu(){
+    return (
+        <div>
+            <a>Plantaciones</a>
+            <a>Estadisticas</a>
+        </div>
+    );
+}
